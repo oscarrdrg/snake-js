91d11ca30b3dbaaab72b4070963c5d80a62cd5dd
Trolex221 <polloryanxd@gmail.com>
2022-03-13 14:33:31 +0100

    master

---
+++ snake.js
@@ -0,0 +1,172 @@
+// constantes del juego
+const COLUMNAS = 40
+const FILAS = 30
+const LADO = 20
+const ANCHO_CANVAS = COLUMNAS * LADO
+const ALTO_CANVAS = FILAS * LADO
+
+
+// variables de entorno html
+let canvas
+
+// variables de escenario
+let serpiente
+let comida
+
+//variables de control
+let arriba, derecha, izquierda, abajo
+
+
+
+function setup() {
+    frameRate(15)
+    canvas = createCanvas(ANCHO_CANVAS, ALTO_CANVAS)
+    windowResized()
+    serpiente = new Serpiente()
+    posicionarComida()
+    arriba = createVector(0, -1)
+    abajo = createVector(0, 1)
+    derecha = createVector(1, 0)
+    izquierda = createVector(-1, 0)
+
+}
+
+function windowResized() {
+
+    let escala = windowWidth / width
+
+    if (escala >= 1) {
+
+        return
+
+    }
+    canvas.style("width", width * escala + "px")
+    canvas.style("height", height * escala + "px")
+}
+
+function draw() {
+
+    background("black")
+    serpiente.dibujar()
+    fill("crimson")
+    rect(comida.x * LADO, comida.y * LADO, LADO, LADO)
+    if (serpiente.posicion.dist(comida) == 0) {
+        serpiente.size++
+        posicionarComida()
+        console.log(serpiente.size)
+    }
+}
+
+function keyPressed() {
+
+    if (!isLooping()) {
+        juegoNuevo()
+    }
+
+    switch (keyCode) {
+        case UP_ARROW:
+            if (serpiente.cola.length && serpiente.aceleracion == abajo) {
+                break
+            }
+            serpiente.aceleracion = arriba
+            break;
+        case RIGHT_ARROW:
+            if (serpiente.cola.length && serpiente.aceleracion == izquierda) {
+                break
+            }
+            serpiente.aceleracion = derecha
+            break;
+        case DOWN_ARROW:
+            if (serpiente.cola.length && serpiente.aceleracion == arriba) {
+                break
+            }
+            serpiente.aceleracion = abajo
+            break;
+        case LEFT_ARROW:
+            if (serpiente.cola.length && serpiente.aceleracion == derecha) {
+                break
+            }
+            serpiente.aceleracion = izquierda
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+function posicionarComida() {
+
+    comida = createVector(int(random(COLUMNAS)), int(random(FILAS)))
+}
+
+function juegoNuevo() {
+
+    serpiente = new Serpiente()
+    posicionarComida()
+    loop()
+
+}
+
+function juegoTerminado() {
+
+    if (serpiente.sistemaDeChoques()) {
+        textAlign(CENTER, CENTER)
+        textSize(50)
+        text("YOU LOSE", width / 2, height / 2)
+        noLoop()
+
+    }
+
+}
+
+function Serpiente() {
+
+    this.posicion = createVector(COLUMNAS / 2, FILAS / 2)
+    this.aceleracion = createVector()
+    this.cola = []
+    this.size = 0
+    this.sistemaDeChoques = function () {
+        if (this.posicion.x < 0 || this.posicion.y < 0) {
+            return true;
+        }
+
+        if (this.posicion.x >= COLUMNAS || this.posicion.y >= FILAS) {
+            return true;
+        }
+
+        for (const c of this.cola) {
+            if (this.posicion.equals(c)) {
+
+                return true
+            }
+
+        }
+
+        return false;
+    }
+    this.dibujar = function () {
+
+        fill("white")
+        rect(constrain(this.posicion.x, 0, COLUMNAS - 1) * LADO, constrain(this.posicion.y, 0, FILAS - 1) *
+            LADO, LADO, LADO)
+
+        for (const c of this.cola) {
+
+            rect(constrain(c.x, 0, COLUMNAS - 1) * LADO, constrain(c.y, 0, FILAS -
+                1) * LADO, LADO, LADO)
+
+        }
+        textAlign(CENTER, CENTER)
+        textSize(15)
+        text("SCORE: " + serpiente.size, width/20, height/40)
+        juegoTerminado()
+
+        this.cola.push(this.posicion.copy())
+        if (this.cola.length > this.size) {
+            this.cola.splice(0, 1)
+        }
+        this.posicion.add(this.aceleracion)
+    }
+
+}
